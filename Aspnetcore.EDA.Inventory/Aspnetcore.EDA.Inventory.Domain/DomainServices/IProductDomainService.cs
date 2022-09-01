33559d6515a404aee225d18e87aa9e5619355549
Thiago Chagas <chagas.bass@gmail.com>
2022-09-01 16:37:43 -0300

    Delete Aspnetcore.EDA.Inventory directory

--- Aspnetcore.EDA.Inventory/Aspnetcore.EDA.Inventory.Domain/DomainServices/IProductDomainService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Aspnetcore.EDA.Inventory.Domain.Entities;
-
-namespace Aspnetcore.EDA.Inventory.Domain.DomainServices
-{
-    public interface IProductDomainService
-    {
-        Task ChangeQuantityAsync(Product product);
-    }
-}
